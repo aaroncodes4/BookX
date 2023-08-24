fe80db66de7a81206e634785c9a3bc33010599d3
Aaron <aaronaparaku026@gmail.com>
2023-08-24 19:38:14 +0000

    update 1

--- screens/BookingScreen.js
+++ screens/BookingScreen.js
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View, Pressable, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Image,
+  FlatList,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import Colors from "../constants/Colors";
+import data from "../data";
+import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const BookingScreen = () => {
   const navigation = useNavigation();
+  const Data = data;
+  const {width, height} = Dimensions.get('window');
   return (
     <SafeAreaView style={{ marginHorizontal: 18 }}>
       <Pressable
@@ -25,34 +39,88 @@ const BookingScreen = () => {
         <Ionicons name="search" size={24} color="white" />
       </Pressable>
 
-      <View style={{ marginVertical: 15 }}>
-        <View
-          style={{
-            backgroundColor: Colors.secondary,
-            padding: 18,
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 30,
-          }}
-        >
-          <View style={styles.imgcont}>
-            <Image 
-              source={require('../assets/obd.jpg')}
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <Pressable
+              onPress={() =>
+                navigation.navigate("Event", {
+                  eventName: item.event,
+                  eventDate: item.date,
+                  eventTime: item.time,
+                  eventVenue: item.venue,
+                  eventRate: item.rate,
+                })
+              }
               style={{
-                width: '100%',
-                height: '100%',
-                resizeMode: 'cover',
+                marginVertical: 10,
+                flexDirection: "row",
+                backgroundColor: "#fff",
               }}
-            />
-          </View>
-          <View>
-            <Text style={{fontSize: 25, color: '#fff', fontWeight: 700}}>All Black party</Text>
-            <Text style={{fontSize: 18, color: '#fff', fontWeight: 500}}>Mandella Loungue</Text>
-            <Text style={{fontSize: 18, color: '#fff', fontWeight: 500}}>22nd August, 2023</Text>
-            <Text style={{fontSize: 18, color: '#fff', fontWeight: 500}}>GHC 20</Text>
-          </View>
-        </View>
-      </View>
+            >
+         
+                <View style={styles.imgcont}>
+                  <Image
+                    source={require("../assets/obd.jpg")}
+                    style={{
+                      width: "100%",
+                      height: height/4,
+                      width: width - 280,
+                    }}
+                  />
+                </View>
+                <View style={{padding: 10}}>
+                  <View style={{flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}}>
+                    <Text
+                      style={{ width: 200, fontSize: 25, color: "#000", fontWeight: 700 }}
+                    >
+                      {item.event}
+                    </Text>
+                    <Entypo name="heart-outlined" size={24} color="red" />
+                  </View>
+                  <View style={{flexDirection: 'row', alignContent: 'center', gap: 10, marginTop: 7}}>
+                    <MaterialIcons name="stars" size={24} color="black" />
+                    <Text style={{fontSize: 20, fontWeight: 500}}>{item.rate}</Text>
+                    <View style={{backgroundColor: Colors.primary, borderRadius: 5, width: 100}}>
+                      <Text style={{textAlign: 'center', fontSize: 20}}>Popular</Text>
+                    </View>
+                  </View>
+                  <Text
+                    style={{ fontSize: 18, color: "#000", fontWeight: 500 }}
+                  >
+                    {item.venue}
+                  </Text>
+                  <Text
+                    style={{ fontSize: 18, color: "#000", fontWeight: 500 }}
+                  >
+                    {item.date}
+                  </Text>
+                  <Text
+                    style={{ fontSize: 18, color: "#000", fontWeight: 500 }}
+                  >
+                    {item.time}
+                  </Text>
+
+                  <Text
+                    style={{ fontSize: 18, color: "#000", fontWeight: 500 }}
+                  >
+                    {item.price}
+                  </Text>
+                  <Text
+                    style={{ fontSize: 18, color: "grey", fontWeight: 500 }}
+                  >
+                    Contact
+                  </Text>
+                  <TouchableOpacity onPress={() => navigation.navigate("Pay")} style={{backgroundColor: "grey", padding: 6, marginTop: 10}}>
+                    <Text style={{fontSize: 25, color: '#fff', textAlign: 'center'}}>Book now</Text>
+                  </TouchableOpacity>
+                </View>
+            </Pressable>
+          );
+        }}
+      />
     </SafeAreaView>
   );
 };
@@ -60,10 +128,5 @@ const BookingScreen = () => {
 export default BookingScreen;
 
 const styles = StyleSheet.create({
-  imgcont: {
-    width: 150,
-    height: 100,
-    backgroundColor: Colors.light,
-    overflow: "hidden",
-  },
+
 });
